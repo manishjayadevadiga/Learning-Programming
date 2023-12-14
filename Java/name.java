597626caebf6931faf372b027b89919a65336d7e
Manish <manishjayadevadiga@gmail.com>
2023-12-14 22:16:26 +0530

    second commit

---
+++ Java/name.java
@@ -0,0 +1,12 @@
+package Java;
+
+import java.util.Scanner;
+
+public class name {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your name");
+        String a = sc.nextLine();
+        System.out.println("Your Name is " + a);
+    }
+}
