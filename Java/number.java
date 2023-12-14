fd2dd683836041fe1f1043e45cf1dccf65e1fe3e
Manish <manishjayadevadiga@gmail.com>
2023-12-14 22:22:24 +0530

    third

---
+++ Java/number.java
@@ -0,0 +1,12 @@
+package Java;
+
+import java.util.Scanner;
+
+public class number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int a = sc.nextInt();
+        System.out.println("entered number from user is " + a);
+    }
+}
